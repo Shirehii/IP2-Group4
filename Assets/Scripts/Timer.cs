d8834f43e061896b823094cc952a26fe1f2a16d3
Shirehii <daigoukaruna@gmail.com>
2021-02-23 19:10:11 +0200

    timer added, pause feature added
    press p, and the game pauses

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text timerText;
+
+    private float startTime;
+    private float t;
+
+    string minutes;
+    string seconds;
+
+    void Start()
+    {
+        timerText = GameObject.Find("TimerText").GetComponent<Text>();
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        //general timer stuff (counting up)
+        t = Time.time - startTime;
+
+        minutes = ((int)t / 60).ToString();
+        seconds = (t % 60).ToString("f1");
+
+        timerText.text = minutes + ":" + seconds;
+
+        //for pausing the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            print("aaaaaaaa");
+            if (Time.timeScale > 0)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
