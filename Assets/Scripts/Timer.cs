45bebe19caf56b663bd9582983bee021fbb165db
Shirehii <daigoukaruna@gmail.com>
2021-02-23 19:43:02 +0200

    added a pause modal

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
+public class Timer : MonoBehaviour //timer script. also takes care of game pausing/unpausing because i didn't want to make a separate script haha
 {
     private Text timerText;
 
@@ -13,10 +13,18 @@ public class Timer : MonoBehaviour
     string minutes;
     string seconds;
 
+    //variables below this comment are not directly linked to the timer, but are for pausing the game
+    private GameObject PauseModal;
+
     void Start()
     {
+        //get the timer-related variables
         timerText = GameObject.Find("TimerText").GetComponent<Text>();
         startTime = Time.time;
+
+        //get the pausing-unpausing related variables
+        PauseModal = GameObject.Find("PauseModal");
+        PauseModal.SetActive(false);
     }
 
     void Update()
@@ -32,14 +40,15 @@ public class Timer : MonoBehaviour
         //for pausing the game
         if (Input.GetKeyDown(KeyCode.P))
         {
-            print("aaaaaaaa");
             if (Time.timeScale > 0)
             {
+                PauseModal.SetActive(true);
                 Time.timeScale = 0;
             }
             else
             {
                 Time.timeScale = 1;
+                PauseModal.SetActive(false);
             }
         }
     }
