15176f8bc0cdc947019eb07e4e6d31e2601efa51
Jonathan257 <62680475+Jonathan257@users.noreply.github.com>
2021-03-01 13:56:31 +0000

    Enemy Update + Spawning!!
    Enemies now spawn randomly around map over a certain time frame, enemies also follow the crystal NOT the players.

---
+++ Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while(enemyCount < 10)
+        {
+            xPos = Random.Range(-8, 8);
+            zPos = Random.Range(1, -11);
+            Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(2.5f);
+            enemyCount += 1;
+        }
+    }
+}
