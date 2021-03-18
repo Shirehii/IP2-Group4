f44d5331e215be38bd6a23ce299186b64f1ba6be
Shirehii <daigoukaruna@gmail.com>
2021-03-18 21:00:47 +0200

    event manager added
    currently only has an event that slows down your movement speed

---
+++ Assets/Scripts/ManageEvents.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageEvents : MonoBehaviour
+{
+    private PlayerMovement pM1;
+    private PlayerMovement pM2;
+
+    private bool running = false;
+
+    void Start()
+    {
+        pM1 = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerMovement>();
+        pM2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerMovement>();
+
+        running = true;
+        StartCoroutine(StartEvents());
+    }
+
+    void Update()
+    {
+        if (!running)
+        {
+            running = true;
+            StartCoroutine(StartEvents());
+        }
+    }
+
+    IEnumerator StartEvents()
+    {
+        yield return new WaitForSeconds(10f);
+        pM1.horizontalSpeed /= 2;
+        pM1.verticalSpeed /= 2;
+        pM2.horizontalSpeed /= 2;
+        pM2.verticalSpeed /= 2;
+        yield return new WaitForSeconds(10f);
+        pM1.horizontalSpeed *= 2;
+        pM1.verticalSpeed *= 2;
+        pM2.horizontalSpeed *= 2;
+        pM2.verticalSpeed *= 2;
+
+        running = false;
+    }
+}
