e81feeec064053702a0c9fdda49f5316b8609579
Shirehii <daigoukaruna@gmail.com>
2021-04-03 21:40:13 +0300

    replaced ability bar sprites & added indicator arrow assets

--- Assets/Scripts/PlayerUIManager.cs
+++ Assets/Scripts/PlayerUIManager.cs
@@ -99,6 +99,7 @@ public class PlayerUIManager : MonoBehaviour
         {
             Vector3 temp = transform.localScale;
             temp.x = pGL.abilityBar / 10;
+            temp.y = 2;
             abilityBar.localScale = temp;
 
             //abilityBar.localScale.x = pGL.abilityBar / 10;
@@ -106,7 +107,7 @@ public class PlayerUIManager : MonoBehaviour
         }
         else if (pGL.abilityBar >= 10)
         {
-            abilityBar.localScale = new Vector3(1, 1, 1);
+            abilityBar.localScale = new Vector3(1, 2, 1);
         }
 
         //switch statement for adjusting the quantity of the ammo ui
